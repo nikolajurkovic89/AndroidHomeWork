d0af16bb03c1d5eebc5d68c518435d9cd8ba767c
Nikola Jurkovic <jurkovicnikola89@gmail.com>
2021-11-26 12:58:10 +0100

    add screen with compose

--- app/src/main/java/com/example/androidhomework/MainActivity.kt
+++ app/src/main/java/com/example/androidhomework/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.androidhomework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupButton()
+    }
+
+    private fun setupButton() {
+        val play_button = findViewById<Button>(R.id.play_video)
+
+        play_button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(this@MainActivity, MainComposeActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
